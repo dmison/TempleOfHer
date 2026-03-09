1ea27ddca6f547e5929ac128ae8456d5349b1b81
Dana Mison <dana.jadzia.mison@gmail.com>
2026-03-09 13:59:30 +1000

    first person prototyping

---
+++ Scripts/FPSController.gd
@@ -0,0 +1,86 @@
+class_name PlayerController
+extends CharacterBody3D
+
+@export_group("Movement")
+@export var max_speed : float = 4.0
+@export var acceleration : float = 20.0
+@export var breaking : float = 20.0
+@export var air_acceleration : float = 4.0
+@export var jump_force : float = 5.0
+@export var gravity_modifier : float = 1.5
+@export var max_run_speed : float = 6.0
+
+@export_group("Camera")
+@export var look_sensitivity : float = 0.005
+@export var look_invert : bool = true
+var camera_look_input : Vector2
+
+@onready var camera : Camera3D = $Camera3D
+@onready var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity") * gravity_modifier
+@onready var mouse_active : bool = false
+var is_running : bool = false
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _physics_process(delta):
+	#apply gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# toggle mouse
+	if Input.is_action_just_pressed('ui_cancel'):
+		mouse_active = not mouse_active
+		
+	if mouse_active:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	if not mouse_active:
+		# jumping
+		if Input.is_action_pressed("jump") and is_on_floor():
+			velocity.y = jump_force
+	
+		# movement
+		var move_input = Input.get_vector("move_left","move_right","move_forward","move_back")
+		var move_dir = (transform.basis * Vector3(move_input.x, 0, move_input.y)).normalized()
+		
+		# running 
+		is_running = Input.is_action_pressed("sprint")
+		var target_speed = max_speed
+		if is_running:
+			target_speed = max_run_speed
+			var run_dot = -move_dir.dot(transform.basis.z)
+			run_dot = clamp(run_dot, 0.0, 1.0)
+			move_dir *= run_dot
+		
+		var current_smoothing = acceleration
+		if not is_on_floor():
+			current_smoothing = air_acceleration
+		elif not move_dir:
+			current_smoothing = breaking
+			
+		var target_vel = move_dir * target_speed
+	
+		velocity.x = lerp(velocity.x, target_vel.x, current_smoothing*delta)
+		velocity.z = lerp(velocity.z, target_vel.z, current_smoothing*delta)
+	move_and_slide()
+	
+	# camera look
+	rotate_y(-camera_look_input.x*look_sensitivity)
+
+	if(look_invert):
+		camera.rotate_x(camera_look_input.y*look_sensitivity)
+	else:
+		camera.rotate_x(-camera_look_input.y*look_sensitivity)
+		
+	
+	camera.rotation.x = clamp(camera.rotation.x, -1.5, 1.5)
+	camera_look_input = Vector2.ZERO
+	
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and not mouse_active:
+		camera_look_input = event.relative
+	
---
+++ Scripts/InteractionController.gd
@@ -0,0 +1,18 @@
+extends RayCast3D
+
+@onready var interact_prompt_label : Label = $"../InteractionPromptLabel"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var object = get_collider()
+	interact_prompt_label.text = ""
+	
+	if object and object is InteractableObject:
+		if object.can_interact == false:
+			return
+			
+		interact_prompt_label.text = "[E] "+object.interact_prompt
+		
+		if Input.is_action_just_pressed("interact"):
+			object._interact()
